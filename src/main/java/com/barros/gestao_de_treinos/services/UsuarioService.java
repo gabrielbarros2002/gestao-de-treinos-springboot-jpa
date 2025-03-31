359cc64ff8aef54ef7b8118e009f0103eec589f4
Gabriel Otávio de Barros <gabriel.barros@kugel.com.br>
2025-03-31 08:38:58 -0300

    - Adição do insert de Usuario

--- src/main/java/com/barros/gestao_de_treinos/services/UsuarioService.java
+++ src/main/java/com/barros/gestao_de_treinos/services/UsuarioService.java
@@ -22,4 +22,8 @@ public class UsuarioService {
         Optional<Usuario> obj = respository.findById(id);
         return obj.get();
     }
+
+    public Usuario insert(Usuario obj) {
+        return respository.save(obj);
+    }
 }
