c8e2b63a7249edc8490cd76ee72a5c9f3f4b14e3
Gabriel Otávio de Barros <gabriel.barros@kugel.com.br>
2025-05-04 00:11:14 -0300

    - Adição do método "handleValidationExceptions()"

--- src/main/java/com/barros/gestao_de_treinos/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/barros/gestao_de_treinos/resources/exceptions/ResourceExceptionHandler.java
@@ -3,12 +3,15 @@ package com.barros.gestao_de_treinos.resources.exceptions;
 import com.barros.gestao_de_treinos.services.exceptions.DatabaseException;
 import com.barros.gestao_de_treinos.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -29,5 +32,25 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
                 request.getRequestURI());
         return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> handleValidationExceptions(ConstraintViolationException ex, HttpServletRequest request) {
+        String erro = "Erro de validação";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String message = ex.getConstraintViolations()
+                .stream()
+                .map(this::formatViolation)
+                .collect(Collectors.joining(" | "));
+        StandardError err = new StandardError(Instant.now(), status.value(), erro, message,
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    private String formatViolation(ConstraintViolation<?> violation) {
+        String propertyPath = violation.getPropertyPath().toString();
+        String parameterName = propertyPath.substring(propertyPath.lastIndexOf('.') + 1);
+
+        return String.format("%s: %s.", parameterName, violation.getMessage());
     }
 }
